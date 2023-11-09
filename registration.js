f2a2115f31f6749653ff854d8bac2a090dbe3d3d
Firdavs3009 <148878605+Firdavs3009@users.noreply.github.com>
2023-11-09 17:44:46 +0500

    Add files via upload

---
+++ registration.js
@@ -0,0 +1,65 @@
+describe('UZD+',  () => {
+    it('main page', async  () => {
+         await $('android.widget.ImageView').click()
+         await browser.pause(2000);
+         await $('android.widget.EditText').addValue('933850046')
+         await browser.pause(2000);
+         await $('android.widget.Button').click()
+         await browser.pause(2000);
+         await $('android.widget.EditText').addValue('769042')
+
+    })
+
+
+    it('main page', async  () => {
+        await $('android.widget.ImageView').click()
+        await browser.pause(2000);
+        await $('android.widget.EditText').addValue('         ')
+        await browser.pause(2000);
+        await $('android.widget.Button').click()
+        await browser.pause(2000);
+        await $('android.widget.EditText').addValue('769042')
+    })
+
+
+
+    it('main page', async  () => {
+        await $('android.widget.ImageView').click()
+        await browser.pause(2000);
+        await $('android.widget.EditText').addValue('933850046')
+        await browser.pause(2000);
+        await $('android.widget.Button').click()
+        await browser.pause(2000);
+        await $('android.widget.EditText').addValue('624234')
+    })
+
+
+
+    it('main page', async  () => {
+        await $('android.widget.ImageView').click()
+        await browser.pause(2000);
+        await $('android.widget.EditText').addValue('933850046')
+        await browser.pause(2000);
+        await $('android.widget.Button').click()
+        await browser.pause(2000);
+        await $('android.widget.EditText').addValue('769042')
+        await browser.pause(2000);
+        await $('android.view.View').click()
+        await browser.pause(2000);
+        await $('//android.widget.EditText').addValue(tursunov)
+        await browser.pause(2000);
+        await $('//android.widget.ImageView[@content-desc="Военный"]').click()
+        await $('//android.widget.ImageView[@content-desc="Детектив"]').click()
+        await $('//android.widget.ImageView[@content-desc="Биография"]').click()
+        await $('//android.widget.ImageView[@content-desc="Криминал"]').click()
+        await $('//android.widget.ImageView[@content-desc="Исторический"]').click()
+        await browser.pause(2000); 
+        await $('android.widget.Button').click()
+        driver.touchPerform([
+                    { action: 'press', options: { x: 840, y: 826 }},    prod>test,
+                    { action: 'wait', options: { ms: 5000 }},
+                    { action: 'release' }
+                  ]);
+
+    })
+}) 
